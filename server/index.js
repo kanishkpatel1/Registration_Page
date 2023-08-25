c02b6876c0667858ca8907f45825cabd36767ebb
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2023-08-25 06:43:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: [""],
     methods: ["GET", "POST"],
     credentials: true
 }));
